572f4b8809e99f836a060559a0e82e0deb75ef54
Ventura Lucena Martínez <44477740+VenturaLM@users.noreply.github.com>
2020-12-01 11:00:04 +0100

    Add files via upload

---
+++ Assignment 5/Part 2/main.cpp
@@ -0,0 +1,14 @@
+/*
+*	File:	main.cpp
+*	Author:	Ventura Lucena Martínez
+*	Email:	i72lumav@uco.es
+*/
+
+#include "n_queens_las_vegas.hpp"
+
+int main()
+{
+	nQueensLasVegas();
+
+	return 0;
+}
---
+++ Assignment 5/Part 2/n_queens_las_vegas.hpp
@@ -0,0 +1,12 @@
+/*
+*	File:	n_queens_las_vegas.hpp
+*	Author:	Ventura Lucena Martínez
+*	Email:	i72lumav@uco.es
+*/
+
+#ifndef __N_QUEENS_LAS_VEGAS__
+#define __N_QUEENS_LAS_VEGAS__
+
+void nQueensLasVegas();
+
+#endif
